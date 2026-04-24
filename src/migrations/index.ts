3784c4b8d9f5426870e237377a8f7c304b91e651
mels <chanatan@outlook.com>
2026-04-24 21:06:17 +0700

    feat: migrate new changes

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20250929_111647 from './20250929_111647';
 import * as migration_20260422_095934 from './20260422_095934';
 import * as migration_20260422_103108 from './20260422_103108';
+import * as migration_20260424_140548 from './20260424_140548';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20260422_103108.up,
     down: migration_20260422_103108.down,
-    name: '20260422_103108'
+    name: '20260422_103108',
+  },
+  {
+    up: migration_20260424_140548.up,
+    down: migration_20260424_140548.down,
+    name: '20260424_140548'
   },
 ];
